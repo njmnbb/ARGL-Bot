82ed08e4b84cde2f918e5058d47ac600298c884e
njmnbb@gmail.com <njmnbb@gmail.com>
2022-10-14 23:29:45 -0500

    let the argl commence

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
+// Create a new client instance
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessages
+    ]
+});
+
+let isTimerComplete = true;
+
+// Detect when a message is sent and check for argl-iness
+client.on('messageCreate', (message) => {
+
+    if(message.content.toUpperCase().includes('ARGL') && !message.author.bot && isTimerComplete) {
+        message.reply(`@everyone \n\n We have a genuine "argl" in the chat. Remain calm!`);
+        isTimerComplete = false;
+        setTimeout(() => isTimerComplete = true, 1200000);
+    }
+});
+
+client.login(token);
