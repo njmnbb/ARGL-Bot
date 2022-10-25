87f82279d3dcf28bbab6207e511430c253fc5e2d
njmnbb@gmail.com <njmnbb@gmail.com>
2022-10-25 17:16:26 -0500

    dumb data types

--- test-schema.js
+++ test-schema.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
     discordId: {
-        type: Number,
+        type: String,
         required: true
     },
     displayName: {
