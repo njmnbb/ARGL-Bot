67dc7da06b6f7a7dc7e3bcf3fddef84d70e13772
njmnbb <njmnbb@gmail.com>
2022-10-25 15:46:15 +0000

    updating file name

---
+++ argl-index.js
@@ -0,0 +1,25 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessages
+    ]
+});
+
+let isTimerComplete = true;
+
+// Detect when a message is sent and check for argl-iness
+client.on('messageCreate', (message) => {
+
+    if(message.content.toUpperCase().includes('ARGL') && !message.author.bot && isTimerComplete) {
+        message.reply(`@everyone \n\nWe have a genuine "argl" in the chat. Remain calm!\n\nBut don't go laughing your pants off just yet because you need to wait **20 more minutes** before the next "argl" can be notified!`);
+        isTimerComplete = false;
+        setTimeout(() => isTimerComplete = true, 1200000);
+    }
+});
+
+client.login(token);
