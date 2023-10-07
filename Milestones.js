3fe76e03c0e84f2f50123025f35a5a71e9135aa5
njmnbb@gmail.com <njmnbb@gmail.com>
2023-10-07 16:06:08 -0500

    Changing the milestone back to the funny number

--- Milestones.js
+++ Milestones.js
@@ -1,4 +1,4 @@
 module.exports = [
     {id: 1, score: 50, message: 'you just hit the ripe old score of 50! \n\nNothing special happens. \nThere is no prize. \nThere is no intrinsic value to the score. \nFuck you.', gif: 'https://media.tenor.com/Wf52Cu03jAoAAAAC/happy50th-birthday-hbd.gif'},
-    {id: 2, score: 144, message: 'you just hit a cool score of 69. Nice.', gif: 'https://media.giphy.com/media/3i4Prsb5uTZArI7fI4/giphy.gif'}
+    {id: 2, score: 69, message: 'you just hit a cool score of 69. Nice.', gif: 'https://media.giphy.com/media/3i4Prsb5uTZArI7fI4/giphy.gif'}
 ]
