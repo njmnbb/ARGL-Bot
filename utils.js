3977335e4234ab3c33171e566243c591e1dbe3e3
njmnbb@gmail.com <njmnbb@gmail.com>
2024-11-05 13:15:01 -0600

    Adding anonymous first and second place masking, adding informational text about the new change

--- utils.js
+++ utils.js
@@ -4,12 +4,11 @@ async function createScoreboard(season, showTotalScores) {
     let userList = await UserSchema.findAndSortAllUsers(season);
     let displayUserList = `\n## SEASON ${season} SCORES\n`;
 
-    userList.forEach((user) => {
-        if(!user.isBanned) {
-            displayUserList += `**${user.displayName}**: ${user.seasonScores[0].score} ${showTotalScores ? '(' + user.score + ' total)' : ''}\n`;
+    userList.forEach((user, i) => {
+        if (!user.isBanned) {
+            displayUserList += `**${isUserInFirstOrSecondPlace(i) ? '🤫' : user.displayName}**: ${isUserInFirstOrSecondPlace(i) ? '🤫' : user.seasonScores[0].score} ${showTotalScores ? '(' + user.score + ' total)' : ''}\n`;
         }
     });
-
     return displayUserList;
 }
 
@@ -17,6 +16,14 @@ function getNumberWithOrdinal(n) {
     var s = ["th", "st", "nd", "rd"],
         v = n % 100;
     return n + (s[(v - 20) % 10] || s[v] || s[0]);
+}
+
+function isUserInFirstOrSecondPlace(i) {
+    if (i === 0 || i === 1) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 module.exports = { createScoreboard, getNumberWithOrdinal };
